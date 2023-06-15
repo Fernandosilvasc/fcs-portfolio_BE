16cb6c854de2506ee01d8e5982c5b7d49215137c
Fernandosilvasc <fernando_silvasc@hotmail.com>
2023-06-15 13:04:13 -0700

    chore: :wrench: Include URL param to server config file

--- config/env/production/server.ts
+++ config/env/production/server.ts
@@ -1,7 +1,7 @@
 
 export default ({env}) => ({
   proxy: true,
-  url: env('APP_URL'), // Sets the public URL of the application.
+  url: env('RENDER_EXTERNAL_URL'), // Sets the public URL of the application.
   app: {
     keys: env.array('APP_KEYS')
   },
