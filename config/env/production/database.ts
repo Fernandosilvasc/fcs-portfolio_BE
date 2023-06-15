3122abaf219e0dedee138bc2eb3df698d674d1a0
Fernandosilvasc <fernando_silvasc@hotmail.com>
2023-06-14 21:36:25 -0700

    chore: :wrench: Set the production config files

---
+++ config/env/production/database.ts
@@ -0,0 +1,21 @@
+
+
+const parse = require("pg-connection-string").parse;
+
+const {host, port, database, user, password} = parse(
+  process.env.DATABASE_URL
+);
+
+export default ({env}) => ({
+  connection: {
+    client: 'postgres',
+    connection: {
+      host,
+      port,
+      database,
+      user,
+      password
+    },
+    debug: false,
+  },
+});
